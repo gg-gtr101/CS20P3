e03f82013fbce03c027be47e5dded53e6277a479
1100111928 <1100111928@L815WD220028.edu.cbe.ab.ca>
2025-11-05 13:39:26 -0700

    1:39PM

--- Chapter 7/src/SkillBuilders/Student.java
+++ Chapter 7/src/SkillBuilders/Student.java
@@ -4,4 +4,22 @@ public class Student
 {
 	private String firstName;
 	private String lastName;
+	private String address;
+	private int stuID;
+	
+	public Student() // Constructor method
+	{
+		firstName = "";
+		lastName = "";
+		address = "###";
+		stuID = 0000000;
+	}
+	
+	public Student(String f, String l, String ad, int ID) // Overload constructor
+	{
+		firstName = f;
+		lastName = l;
+		address = ad;
+		stuID = ID;
+	}
 }
