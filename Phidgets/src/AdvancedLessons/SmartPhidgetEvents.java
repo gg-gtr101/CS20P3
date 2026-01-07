fbed3ac53b7fecb2381fc15dd37da2c29c133888
1100111928 <1100111928@L815WD220028.edu.cbe.ab.ca>
2026-01-07 13:31:24 -0700

    1:31PM

--- Phidgets/src/AdvancedLessons/SmartPhidgetEvents.java
+++ Phidgets/src/AdvancedLessons/SmartPhidgetEvents.java
@@ -4,37 +4,32 @@ import com.phidget22.*;
 
 public class SmartPhidgetEvents 
 {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception 
+    {
 
-        TemperatureSensor tempSensor = new TemperatureSensor();
-        HumiditySensor humiditySensor = new HumiditySensor();
+        DigitalOutput redLED = new DigitalOutput();
 
-        
-        tempSensor.addTemperatureChangeListener(new TemperatureSensorTemperatureChangeListener() {
-            public void onTemperatureChange(TemperatureSensorTemperatureChangeEvent e) {
-                double temp = e.getTemperature();
-                
-                if (temp > 21.0) {
-                    System.out.println("Current Temperature: " + temp + "°C");
-                } else {
-                    System.out.println("Room is too cold");
-                }
-            }
-        });
+        redLED.setHubPort(1);
+        redLED.setIsHubPortDevice(true);
+        redLED.open(1000);
 
-        humiditySensor.addHumidityChangeListener(new HumiditySensorHumidityChangeListener() {
-            public void onHumidityChange(HumiditySensorHumidityChangeEvent e) {
-                System.out.println("Humidity: " + e.getHumidity() + "% RH");
-            }
-        });
-
-        tempSensor.open(1000);
-        humiditySensor.open(1000);
-        
-        while (true) 
+        //
+        for (int i = 0; i < 5; i++) 
         {
-            Thread.sleep(150);
+            
+            for (double brightness = 0.0; brightness <= 1.0; brightness += 0.05) 
+            {
+                redLED.setDutyCycle(brightness);
+                Thread.sleep(50);
+            }
+
+            for (double brightness = 1.0; brightness >= 0.0; brightness -= 0.05) 
+            {
+                redLED.setDutyCycle(brightness);
+                Thread.sleep(50);
+            }
         }
+        
+        System.out.println("Brightness Demo Finished.");
     }
 }
-	
